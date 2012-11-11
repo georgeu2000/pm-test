88e4a3f1a3b3c8c2e152a75989a7cb0cf39e01d9
George <george@boost-media.com>
2012-11-11 04:18:09 -0800

    Remove old files

--- app/assets/javascripts/widget.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $( "#dialog" ).dialog({ autoOpen: false });
-  $( "#opener" ).click(function() {
-    $( "#dialog" ).dialog( "open" );
-    $( "#dialog" ).load('/articles');
-  });
-});
